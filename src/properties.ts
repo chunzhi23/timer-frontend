2f0e1739154202a421cb861de8bbfdb73bbaab99
chunzhi23 <lzhongm05@gmail.com>
2025-07-27 22:00:03 +0900

    feat: deploy using dockerfile

--- src/properties.ts
+++ src/properties.ts
@@ -1,7 +1,8 @@
 const START_DATE = new Date("2025-08-04T00:00:00");
 const DUE_DATE = new Date("2027-02-03T00:00:00");
 
-const MESSAGE = "SEE YOU THEN";
+const MESSAGE =
+  "Those who are wise will shine like the brightness of the heavens, and those who lead many to righteousness, like the stars for ever and ever. — Daniel 12:3";
 const PREV_COUNT_MSG = "The countdown starts at 2025.08.04";
 
 export { START_DATE, DUE_DATE, MESSAGE, PREV_COUNT_MSG };
